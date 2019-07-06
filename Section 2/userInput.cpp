c57b4bffbdaded255d2f54561d65afaec73fef10
bhanukaManesha <bhanukamgamage@gmail.com>
2019-07-06 09:35:04 +0800

    Added input and while

---
+++ Section 2/userInput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+    cout << "Enter your name: " << flush;
+
+    string input;
+
+    cin >> input;
+
+    cout << "String you entered : " << input << endl;
+
+    return 0;
+}
---
+++ Section 2/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numberCats = 5;
+
+    string catName = "John";
+
+    cout << "Number of cats : " << catName << ":" << numberCats << endl;
+
+    return 0;
+}
