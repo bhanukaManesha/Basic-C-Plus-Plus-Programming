2822f39aedf453a2e529aaf207903f0ecf27dccb
bhanukaManesha <bhanukamgamage@gmail.com>
2019-07-06 09:55:47 +0800

    Added Array

---
+++ Section 2/array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+    int myArray[10];
+
+    for (int i = 0; i < 10; i++){
+        myArray[i] = i;
+    }
+
+    cout << myArray[3] << endl;
+    return 0;
+}
